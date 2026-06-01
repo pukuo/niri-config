ef96b71cbc45bcf3a57c7b591c1660c00696909c
guolisen <89941460+pukuo@users.noreply.github.com>
2026-06-01 00:25:33 +0000

    更新 disk-config.nix

---
+++ disk-config.nix
@@ -0,0 +1,48 @@
+# disk-config.nix
+{
+  disko.devices = {
+    disk.main = {
+      device = "/dev/sda";          # 改成你的实际磁盘
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "@" = {
+                  mountpoint = "/";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
+                "@nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
+                "@home" = {
+                  mountpoint = "/home";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
+                "@swap" = {
+                  mountpoint = "/.swap";
+                  mountOptions = [ "noatime" ];
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
